6ccb97ef26d112e4ee86cb28b8e7659c2a803a9f
adnan <sthep90@gmail.com>
2025-06-27 20:53:54 +0600

    VulkTools For Tweaking vulkan

---
+++ Vulk.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adnan on 6/27/25.
+//
+
+#include "Vulk.h"
---
+++ Vulk.h
@@ -0,0 +1,16 @@
+//
+// Created by adnan on 6/27/25.
+//
+
+#ifndef VULK_H
+#define VULK_H
+
+
+
+class Vulk {
+
+};
+
+
+
+#endif //VULK_H
