dab3aafb8296608fbd3634152231e90bb46e772e
Rahat Adnan <sthep90@gmail.com>
2025-07-21 23:05:47 +0600

    graphics pipeline (color blending)

--- pipeline/Graphics/GraphicsPipeline.cpp
+++ pipeline/Graphics/GraphicsPipeline.cpp
@@ -152,6 +152,9 @@ void GraphicsPipeline::setupColorBlending() {
     this->colorBlendStateCreateInfo.attachmentCount = 1;
     this->colorBlendStateCreateInfo.pAttachments = & this->colorBlendAttachmentState;
     this->colorBlendStateCreateInfo.blendConstants[0] = 0.0f;
+    this->colorBlendStateCreateInfo.blendConstants[1] = 0.0f;
+    this->colorBlendStateCreateInfo.blendConstants[2] = 0.0f;
+    this->colorBlendStateCreateInfo.blendConstants[3] = 0.0f;
 }
 
 
