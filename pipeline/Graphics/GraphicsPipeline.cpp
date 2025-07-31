7179889a4ed1e3e8b083c2a80eeaaf536f0ff067
Rahat Adnan <sthep90@gmail.com>
2025-08-01 00:23:06 +0600

    building render pass

--- pipeline/Graphics/GraphicsPipeline.cpp
+++ pipeline/Graphics/GraphicsPipeline.cpp
@@ -173,7 +173,7 @@ void GraphicsPipeline::setupRenderPass() {
     colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
     colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
     colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-    colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED; //* we don't care what previous layout the image was in
     colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
 
 
