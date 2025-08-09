83f855a9b6cab525301b9823d15eb692971f7460
Rahat Adnan <sthep90@gmail.com>
2025-08-10 00:25:35 +0600

    #warning comment added. all warning comments will be resolved ASAP.

--- Render/FrameController.cpp
+++ Render/FrameController.cpp
@@ -85,7 +85,7 @@ void FrameController::recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_
 
     //* BInd with graphics pipeline
     vkCmdBindPipeline(this->CommandBuffers,VK_PIPELINE_BIND_POINT_GRAPHICS,this->graphicsPipeline.getGraphicsPipeline());
-    vkCmdSetScissor(this->CommandBuffers,ZERO,ONE,&this->graphicsPipeline.getViewportScissors().scissor);
+    vkCmdSetScissor(this->CommandBuffers,ZERO,ONE,&this->graphicsPipeline.getScissor());
 
 }
 
