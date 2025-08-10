125d90b1d0b0ef29185f0b2f02d8b1bddc97366b
Rahat Adnan <sthep90@gmail.com>
2025-08-10 11:30:35 +0600

    SDL WindowManager.h

---
+++ external/sdl/WindowManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by STHEP on 8/10/2025.
+//
+
+#include "WindowManager.h"
---
+++ external/sdl/WindowManager.h
@@ -0,0 +1,13 @@
+//
+// Created by STHEP on 8/10/2025.
+//
+
+#ifndef PRACTICEVK_WINDOWMANAGER_H
+#define PRACTICEVK_WINDOWMANAGER_H
+
+
+class WindowManager {
+};
+
+
+#endif //PRACTICEVK_WINDOWMANAGER_H
