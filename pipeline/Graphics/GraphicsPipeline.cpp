3b7a4a568a9b4b7ea5206740eb1c44a43cd5b70f
Rahat Adnan <sthep90@gmail.com>
2025-08-28 21:36:33 +0600

    DEBUGGING DIVIDE BY ZERO ERROR

--- pipeline/Graphics/GraphicsPipeline.cpp
+++ pipeline/Graphics/GraphicsPipeline.cpp
@@ -233,7 +233,7 @@ void GraphicsPipeline::setupRenderPass() {
 
 
 void GraphicsPipeline::createGraphicsPipeline() {
-    VkPipelineShaderStageCreateInfo shaderStages[2]={
+    VkPipelineShaderStageCreateInfo shaderStages[]={
         this->vertexShaderStage,
         this->fragmentShaderStage
     };
